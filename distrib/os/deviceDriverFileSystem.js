74d2cf778450ff16029d7396df2298785ba4a9d2
Paul Ippolito <paul.ippolito2@marist.edu>
2019-12-09 22:23:20 -0500

    file for file system created

---
+++ distrib/os/deviceDriverFileSystem.js
@@ -0,0 +1,10 @@
+///<reference path="../globals.ts" />
+///<reference path="deviceDriver.ts" />
+///<reference path="console.ts" />
+var TSOS;
+(function (TSOS) {
+    class DeviceDriverFileSystem extends TSOS.DeviceDriver {
+    }
+    TSOS.DeviceDriverFileSystem = DeviceDriverFileSystem;
+})(TSOS || (TSOS = {}));
+//# sourceMappingURL=deviceDriverFileSystem.js.map
---
+++ source/os/deviceDriverFileSystem.ts
@@ -0,0 +1,9 @@
+///<reference path="../globals.ts" />
+///<reference path="deviceDriver.ts" />
+///<reference path="console.ts" />
+
+module TSOS {
+    export class DeviceDriverFileSystem extends DeviceDriver{
+        
+    }
+}
